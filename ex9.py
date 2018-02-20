0e0354f3c67f3ab3d7950d7650bd2a7006377976
ankit dwivedi <ankit@oc0158648367.ibm.com>
2018-02-20 21:19:53 +0530

    commit develop

---
+++ ex9.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue wed thu fri sat sun "
+saw = "feb"
+months = "\njan\n%s\nmar\napr\nmay\njune\njul\naug" % saw
+
+print " Here are the days :", days 
+print " Here are the months:", months
+
+print """
+There's something going on here.
+with the three double-quotes.
+We'll be able to typw as much as we like 
+even 4 line if we want , or 5 , or 6.
+"""
+
---
+++ exe3ex1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+print "I will now count chickens:"
+print "Hens", 25 + 30 / 6
+print "Rosters", 100.0-25*3%4
+print 'Now I wil count the eggs:'
+print 3+2+1-5+4%2-1/4+6
+print 'Is it true that 3+2< 5-7?'
+print 3+2<5-7
+print 'what is 3+2?', 3+2
+print 'what is 5-7?', 5-7
+print "oh, that's why its false."
+print "How about some more."
+print " IS it greater?", 5>-2
+print "Is is greater or equal?", 5>=-2
+print "Is is less or equal?", 5 <= -2
